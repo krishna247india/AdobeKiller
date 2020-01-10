0dbd42a6d820a48d4a78ac4b2b4e59d36d174570
krishna247india <58545202+krishna247india@users.noreply.github.com>
2020-01-10 17:51:24 +0530

    First commit.

---
+++ AdobeKill.bat
@@ -0,0 +1,25 @@
+:--------------------------------------
+:Developed by :- krishna247india
+:Download URL :- https://github.com/krishna247india/AdobeKiller.git
+:--------------------------------------
+@echo off
+TASKKILL /F /IM "Creative Cloud.exe" /T
+TASKKILL /F /IM "Adobe Desktop Service.exe" /T
+TASKKILL /F /IM "Adobe CEF Helper.exe" /T
+TASKKILL /F /IM "Adobe Installer.exe" /T
+TASKKILL /F /IM "AdobeUpdateService.exe" /T
+TASKKILL /F /IM "CoreSync.exe" /T
+TASKKILL /F /IM "AdobeIPCBroker.exe" /T
+TASKKILL /F /IM "Adobe Installer.exe" /T
+TASKKILL /F /IM "CCLibrary.exe" /T
+TASKKILL /F /IM "AdobeNotificationClient.exe" /T
+TASKKILL /F /IM "RuntimeBroker.exe" /T
+TASKKILL /F /IM "AdobeIPCBroker.exe" /T
+TASKKILL /F /IM "AdobeUpdateService.exe" /T
+TASKKILL /F /IM "CCXProcess.exe" /T
+TASKKILL /F /IM "backgroundTaskHost.exe" /T
+TASKKILL /F /IM "AGSService.exe" /T
+TASKKILL /F /IM "AGMService.exe" /T
+TASKKILL /F /IM "LogTransport2.exe" /T
+TASKKILL /F /IM "AdobeGCClient.exe" /T
+pause
